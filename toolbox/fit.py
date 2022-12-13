aed92cbc56a871077766f86c0d119e7c21fca2db
Pavel Zwerschke <pavelzw@gmail.com>
2022-12-13 16:12:47 +0100

    Add basic structure of model pipeline

---
+++ toolbox/fit.py
@@ -0,0 +1,54 @@
+from toolbox.data import read_data
+from toolbox.model_pipeline import preprocess_data, create_model, train_model
+
+CONFIG = {
+    # 'model': 'decision_tree',
+    # 'model_params': {},
+
+    'model': 'random_forest',
+    'model_params': {},
+
+    # 'model': 'boosted_tree',
+    # 'model_params': {},
+
+    # 'model': 'mlp',
+    # 'model_params': {},
+    'y_feature': 'rent',
+
+    'features': [
+        'areaSqm',
+        # 'city',
+        'furnish',
+        'propertyType',
+        # 'rent',
+        # 'additionalCosts',
+        # 'deposit',
+        'gender',
+        'matchStatus',
+        'pets',
+        'roommates',
+        'shower',
+        'smokingInside',
+        'toilet',
+    ],
+
+    'random_state': 42,
+
+    'model_selection': 'kfold',
+    'model_selection_params': {
+        'n_splits': 5,
+        'shuffle': True,
+    },
+
+    # 'model_selection': 'train_test_split',
+    # 'model_selection_params': {
+    #     'test_size': 0.2,
+    #     'shuffle': True,
+    # },
+}
+
+
+if __name__ == '__main__':
+    data = preprocess_data(CONFIG)
+    train_model(lambda: create_model(CONFIG), data, CONFIG)
+
---
+++ toolbox/model_pipeline.py
@@ -0,0 +1,65 @@
+import pandas.api.types as ptypes
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.model_selection import KFold, train_test_split
+from sklearn.neural_network import MLPRegressor
+from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeRegressor
+
+from toolbox.data import read_data
+
+
+def preprocess_data(config):
+    # Preprocess data
+    data = read_data('../data/properties.json')
+
+    data['furnish'] = LabelEncoder().fit_transform(data['furnish'])
+    data['propertyType'] = LabelEncoder().fit_transform(data['propertyType'])
+    data['gender'] = LabelEncoder().fit_transform(data['gender'])
+    data['matchStatus'] = LabelEncoder().fit_transform(data['matchStatus'])
+    data['pets'] = LabelEncoder().fit_transform(data['pets'])
+    data['roommates'] = LabelEncoder().fit_transform(data['roommates'])  # todo: this is not a good idea
+    data['shower'] = LabelEncoder().fit_transform(data['shower'])
+    data['smokingInside'] = LabelEncoder().fit_transform(data['smokingInside'])
+    data['toilet'] = LabelEncoder().fit_transform(data['toilet'])
+
+    assert ptypes.is_numeric_dtype(data['rent'])
+    assert ptypes.is_numeric_dtype(data['additionalCosts'])
+    # TODO preprocessing
+    return data[config['features'] + [config['y_feature']]]
+
+
+def create_model(config):
+    # Create model
+    if config['model'] == 'decision_tree':
+        clf = DecisionTreeRegressor(**config['model_params'], random_state=config['random_state'])
+    elif config['model'] == 'random_forest':
+        clf = RandomForestRegressor(**config['model_params'], random_state=config['random_state'])
+    elif config['model'] == 'boosted_tree':
+        clf = GradientBoostingRegressor(**config['model_params'], random_state=config['random_state'])
+    elif config['model'] == 'mlp':
+        clf = MLPRegressor(**config['model_params'], random_state=config['random_state'])
+    else:
+        raise ValueError('Unknown model: {}'.format(config['model']))
+    return clf
+
+
+def train_model(get_model, data, config):
+    X = data[config['features']]
+    y = data['rent']
+    model = None
+    if config['model_selection'] == 'kfold':
+        kf = KFold(**config['model_selection_params'], random_state=config['random_state'])
+        for train_index, test_index in kf.split(X):
+            model = get_model()
+            X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+            y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+            model.fit(X_train, y_train)
+            print(model.score(X_test, y_test))
+    elif config['model_selection'] == 'train_test_split':
+        X_train, X_test, y_train, y_test = train_test_split(**config['model_selection_params'], random_state=config['random_state'])
+        model = get_model()
+        model.fit(X_train, y_train)
+        print(model.score(X_test, y_test))
+    else:
+        raise ValueError('Unknown model selection method: {}'.format(config['model_selection']))
+    return model
